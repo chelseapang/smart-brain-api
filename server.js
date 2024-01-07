2a0bfe817c55b141d97d9725fbe80dd29c6b8567
Chelsea Pang <chelsea.pang96@gmail.com>
2024-01-07 11:31:34 -0800

    setting up register endpoint

--- server.js
+++ server.js
@@ -27,20 +27,33 @@ const database = {
     ]
 }
 
-app.get('/', (req, res)=> {
-    res.send('this is working');
+app.get('/', (req, res) => {
+    res.send(database.users);
 })
 
-app.post('/signin', (req, res)=> {
+app.post('/signin', (req, res) => {
     if (req.body.email === database.users[0].email &&
         req.body.password === database.users[0].password) {
-            res.json('success');
-        } else {
-            res.status(400).json('error logging in');
-        }
+        res.json('success');
+    } else {
+        res.status(400).json('error logging in');
+    }
 })
 
-app.listen(3000, ()=> {
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length - 1]);
+})
+
+app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
 
