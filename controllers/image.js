71f03af37170facc36771f8359df219446d3b0b9
Chelsea Pang <chelsea.pang96@gmail.com>
2024-01-09 14:16:12 -0800

    moving clarifai configuration to backend for better security

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,59 @@
+const { handle } = require("express/lib/application");
+
+const returnClarifaiRequestOptions = (imageUrl) => {
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    //TODO: remove PAT before pushing to github
+    const PAT = '9581081912ff48b2a9b632deff281278';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'cpang5';
+    const APP_ID = 'face-recognition-brain';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';
+    const IMAGE_URL = imageUrl;
+  
+    const raw = JSON.stringify({
+      "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+      },
+      "inputs": [
+        {
+          "data": {
+            "image": {
+              "url": IMAGE_URL
+            }
+          }
+        }
+      ]
+    });
+  
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+      },
+      body: raw
+    };
+  
+    return requestOptions;
+  }
+
+  const handleApiCall = (req, res) => {
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnClarifaiRequestOptions(req.body.input))
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch data from Clarifai API');
+        }
+        return response.json();
+      })
+      .then(data => {
+        res.json(data);
+      })
+      .catch(err => res.status(400).json('unable to work with API'));
+  };
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +66,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage: handleImage
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
 } 
